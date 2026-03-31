89dad62af8c9c650eb10f4eebf1b28a2b377a839
ayush4484 <ayushraj4484@gmail.com>
2026-03-31 20:53:01 +0530

    Create script2.sh

---
+++ script2.sh
@@ -0,0 +1,13 @@
+
+#!/bin/bash
+# Script 2: FOSS Package Inspector # By: Ayush Raj
+
+# Package selection PACKAGE="python3"
+# Check if package is installed
+if dpkg -l | grep -q $PACKAGE; then echo "$PACKAGE is installed."
+dpkg -s $PACKAGE | grep -E 'Version|Maintainer|Description'
+else
+echo "$PACKAGE is not installed."
+fi
+# Package description case $PACKAGE in
+python3) echo "Python: community-driven language used worldwide" ;; esac
